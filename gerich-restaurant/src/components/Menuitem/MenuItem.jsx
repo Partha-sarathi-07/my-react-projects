661acfbbcc264a2f7d1ff6ed4e60311926c88670
sarathi <sarathicharu07@gmail.com>
2024-06-08 20:52:28 +0530

    till intro section

--- gerich-restaurant/src/components/Menuitem/MenuItem.jsx
+++ gerich-restaurant/src/components/Menuitem/MenuItem.jsx
@@ -11,13 +11,13 @@ const MenuItem = ({ title, price, tags }) => (
 
       <div className='app__menuitem-dash' />
 
-      <div className='app__menuitem-name'>
+      <div className='app__menuitem-price'>
         <p className='p__cormorant'>{ price }</p>
       </div>
     </div>
 
     <div className='app__menuitem-sub'>
-      <p className='p__cormorant' style={{ color: '#AAA'}}>{ tags }</p>
+      <p className='p__opensans' style={{ color: '#AAA'}}>{ tags }</p>
     </div>
   </div>
 );
